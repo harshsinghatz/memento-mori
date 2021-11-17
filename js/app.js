ab53d5760acc530c02ca46899024e9ad3d11c37a
Harsh Singh <51085015+hs9085298@users.noreply.github.com>
2021-11-17 11:00:51 +0530

    Design and Implementation

---
+++ js/app.js
@@ -0,0 +1,68 @@
+"using strict";
+
+//Selecting DOM elements
+const dateOfBirth = document.querySelector("#dob");
+const noOfYears = document.querySelector(".selected-years");
+const resetBtn = document.querySelector(".reset");
+
+const displayBlock = document.querySelector(".total-years");
+const about = document.querySelector(".about");
+
+const calcBtn = document.querySelector(".btn");
+//Functions for Calculations
+const getNoOfWeeks = function (dateOfBirth) {
+  const today = new Date().toISOString().slice(0, 10);
+
+  const startDate = dateOfBirth.slice(0, 10);
+  const endDate = today;
+
+  const diffInMs = new Date(endDate) - new Date(startDate);
+  const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+  return Math.round(diffInDays / 7);
+};
+const getWeeksFromYears = function (years) {
+  return Math.round((years * 365) / 7);
+};
+
+//Fucntions for Displaying the Dots
+const displayDots = function (noOfWeeks, numOfInpWeeks) {
+  if (!Number.isNaN(noOfWeeks)) {
+    displayBlock.innerHTML = "";
+    // about.classList.add("hidden");
+    about.style.display = "none";
+    const noOfBoxes = 260;
+    const noOfBlocks = numOfInpWeeks / noOfBoxes;
+    let currNoOfWeeks = 1;
+    for (let block = 0; block < noOfBlocks; block++) {
+      displayBlock.insertAdjacentHTML(
+        "beforeend",
+        "<div class='box-grid'></div>"
+      );
+      const grid = displayBlock.lastChild;
+      for (let i = 1; i <= noOfBoxes; i++) {
+        grid.insertAdjacentHTML(
+          "beforeend",
+          `<div class="week ${
+            currNoOfWeeks <= noOfWeeks ? "active" : ""
+          }"></div>`
+        );
+        currNoOfWeeks++;
+      }
+    }
+  }
+};
+const reset = function () {
+  displayBlock.innerHTML = "";
+  // about.classList.remove("hidden");
+  about.style.display = "flex";
+};
+//Event Handlers
+
+calcBtn.addEventListener("click", (e) => {
+  const weeks = getNoOfWeeks(dateOfBirth.value);
+  const userInputWeeks = getWeeksFromYears(Number(noOfYears.value));
+  displayDots(weeks, userInputWeeks);
+});
+resetBtn.addEventListener("click", () => {
+  reset();
+});
